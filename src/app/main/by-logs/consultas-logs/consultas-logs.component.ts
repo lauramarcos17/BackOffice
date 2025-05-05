ffc766cbe412977b3ddeddbcc269b2d7f7ffc85d
unknown <dhercan391@gmail.com>
2025-05-05 09:24:41 +0200

    Guardado previo antes de tooltip. Restricción de visor logs

--- src/app/main/by-logs/consultas-logs/consultas-logs.component.ts
+++ src/app/main/by-logs/consultas-logs/consultas-logs.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import {MatPaginatorModule} from '@angular/material/paginator';
 import {MatTableModule} from '@angular/material/table';
+import { MiSignalService } from '../../../shared/services/mi-signal.service';
 
 export interface Log{
   error:number;
@@ -11,8 +12,10 @@ export interface Log{
   query: string;
 }
 const ELEMENT_DATA: Log[] = [
-  {error: 1, fecha: new Date(1991, 10, 30), rol:3,cuaderno :'34SEPA',accion:'Lista ordenantes', query: 'update 98293'},
-  
+  {error: 1, fecha: new Date(1991, 10, 30), rol:3,cuaderno :'34SEPA',accion:'Listar ordenantes', query: 'update 34293'},
+  {error: 2, fecha: new Date(1991, 10, 30), rol:2,cuaderno :'34SEPA',accion:'Listar ordenantes', query: 'update 976693'},
+  {error: 3, fecha: new Date(1998, 10, 30), rol:1,cuaderno :'34SEPA',accion:'Listar ordenantes', query: 'update 98293'},
+
 ];
 
 
@@ -22,9 +25,12 @@ const ELEMENT_DATA: Log[] = [
   templateUrl: './consultas-logs.component.html',
 })
 export class ConsultasLogsComponent {
- 
+  misignalService = inject(MiSignalService);
+  rol=this.misignalService.rol;
   displayedColumns: string[] = ['error', 'fecha', 'rol','cuaderno','accion','query'];
   dataSource = ELEMENT_DATA;
+
+
 
 
 
--- src/app/main/by-migrar/by-migrar.component.ts
+++ src/app/main/by-migrar/by-migrar.component.ts
@@ -1,9 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component, computed, inject, signal } from '@angular/core';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatLabel } from '@angular/material/form-field';
 import {MatRadioModule} from '@angular/material/radio';
 import { MigManualComponent } from './mig-manual/mig-manual.component';
 import { MigMasivaComponent } from './mig-masiva/mig-masiva.component';
+import { MiSignalService } from '../../shared/services/mi-signal.service';
 
 @Component({
   selector: 'app-by-migrar',
@@ -12,6 +13,11 @@ import { MigMasivaComponent } from './mig-masiva/mig-masiva.component';
   styleUrl: 'by-migrar.component.css',
 })
 export class ByMigrarComponent {
- 
+
   tipoMigracion=signal<number>(0);
+  misignalService=inject(MiSignalService);
+
+
+  rolEsAdmin = computed(() => this.misignalService.rol() == 1);
+  
  }
